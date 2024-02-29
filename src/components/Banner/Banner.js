a7b178f64fd0d7a054186f579d86a5c7b9b4158e
Nick-Rodrigues <RM551060@fiap.com.br>
2024-02-29 18:07:01 -0300

    Finalização projeto.

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,14 @@
+import { Fragment } from 'react'
 import './Banner.css'
 
 function Banner() {
     return(
-        <header className="banner">
-            <img src="/imagens/banner.png" alt="Banner principal"/>
-        </header>
+        <>              
+            <header className="banner">
+                <img src="/imagens/banner.png" alt="Banner principal"/>
+            </header>
+            <h1>teste</h1>
+        </>
     )
 
 }
--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -1,8 +1,8 @@
 import './Colaborador.css'
 
-const Colaborador = ({nome, imagem, cargo}) => {
+const Colaborador = ({nome, imagem, cargo, corDeFundo}) => {
     return(
-        <div className='colaborador'>
+        <div className='colaborador' style={{ backgroundColor: corDeFundo}}>
             <div>
                 <img src={imagem} alt={nome}/>
             </div>
